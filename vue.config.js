fda60cb2be4fa69003838e67e178d37a6dbb06b9
PeerHenry <peerhenry@gmail.com>
2019-02-16 12:45:58 +0100

    added bundle analyser

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+const config = {
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
+}
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: config // will be merged into webpack, see: https://cli.vuejs.org/guide/webpack.html
 }
